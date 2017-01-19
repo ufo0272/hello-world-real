2597917e75abb55a60eb650236772267bd8e1a6a
ufo0272 <herman.bundy@hotmail.com>
2017-01-19 09:51:18 +0100

    Added hello-world

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunClass run = new RunClass();
+            run.write();
+        }
+    }
+}
---
+++ hello-world/hello-world/RunClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class RunClass
+    {
+        public void write()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
